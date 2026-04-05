f4bc665c46685c5bc2d7c18066b4baf96f6b569e
dbsnen393-cmyk <dbsnen393@github.com>
2026-04-05 20:25:07 +0500

    Don't fail deploy if seeds error

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,4 +5,4 @@ bundle install
 NODE_OPTIONS=--openssl-legacy-provider bundle exec rake assets:precompile
 NODE_OPTIONS=--openssl-legacy-provider bundle exec rake webpacker:compile
 bundle exec rake db:migrate
-bundle exec rake db:seed
+bundle exec rake db:seed || true
